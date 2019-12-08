1e94beb36923b1df6dccc1c868d455fdda863e0c
Firas Moosvi <firas@moosvi.com>
2019-12-07 20:41:32 -0800

    Update init.R

--- init.R
+++ init.R
@@ -1,7 +1,6 @@
 install.packages("ggplot2")
 install.packages("devtools")
-library(devtools)
-install_github('plotly/dashR')
+devtools::install_github('plotly/dashR')
 install.packages("dashHtmlComponents")
 install.packages("dashCoreComponents")
 
