e25d2134a32fce3919078e9ec45eb171fc2a5f37
Firas Moosvi <firas@moosvi.com>
2019-12-08 20:15:26 -0800

    reduce htmlcomponents to 1.0.0

--- init.R
+++ init.R
@@ -47,7 +47,7 @@ install.packages("https://cloud.r-project.org/src/contrib/Archive/future/future_
 # # fiery and friends
 install.packages("https://cloud.r-project.org/src/contrib/routr_0.4.0.tar.gz", type="source", repos=NULL)
 install.packages("https://cloud.r-project.org/src/contrib/fiery_1.1.2.tar.gz", type="source", repos=NULL)
-remotes::install_github("plotly/dash-html-components")
+remotes::install_github("plotly/dash-html-components@v1.0.0")
 remotes::install_github("plotly/dash-core-components@fface5b") # special commit for v1.0.0
 remotes::install_github("plotly/dash-table")
 
